a48db11a667055bfe0ea58f92dff4a65a3a68ab3
Sagar Chand <chandsagar314@gmail.com>
2024-07-15 19:15:42 +0545

    footer grid position changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,15 +6,15 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pt-20 pb-10 relative" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="w-full h-full absolute left-0 bottom-0">
         <Image
           width={100}
           height={100}
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50 "
+          className="w-full h-full opacity-70"
         />
       </div>
 
