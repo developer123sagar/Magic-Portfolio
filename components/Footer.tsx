67fffd6232ab40176e7892625aff2741c1726e28
Sagar Chand <chandsagar314@gmail.com>
2024-07-15 18:53:27 +0545

    changes in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,16 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/constants";
 import BorderMagicButton from "./ui/border-magic-button";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
+          width={100}
+          height={100}
           src="/footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50 "
