96a1760f6796cacab8a7fe36661e99a50bdf21b1
Sagar chand <118549436+developer123sagar@users.noreply.github.com>
2025-10-13 08:37:36 +0545

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,6 @@ export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Experience", link: "#experience" },
-  // { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
@@ -157,3 +156,5 @@ export const fadeInAnimationVariants = {
     },
   }),
 };
+
+// testing submodule
