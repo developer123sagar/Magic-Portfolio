c6cf4441501bdfe21dee66dc19f133f0af4f3ffe
Sagar chand <118549436+developer123sagar@users.noreply.github.com>
2026-02-25 23:56:44 +0545

    refactor: update copyright year from 2024 to 2025

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light text-white">
-          Copyright © 2024 Sagar Chand
+          Copyright © 2025 Sagar Chand
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
